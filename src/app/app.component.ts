6c3e184b86f267daae70c83dcc739af3ba716060
Micah Johnson <mjohnson@teleflora.org>
2020-05-06 22:19:51 -0500

    adding dist folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'passwordgenerator';
+  title = 'Password Generator';
+  password = '';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  disable = false;
+
+  onButtonClick(e) {
+    e.preventDefault();
+    const numbers = '1234567890';
+    const letters = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+
+    if(this.includeLetters) {
+      validChars += letters;
+    }
+
+    if (this.includeNumbers) {
+      validChars += numbers
+    }
+
+    if (this.includeSymbols) {
+      validChars += symbols
+    }
+
+    let generatedPW = ''
+
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPW += validChars[index];
+    }
+
+    this.password = generatedPW;
+  }
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+
+        if(!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
 }
